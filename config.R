d8d659a05ae05023ec1386ce75f576378c5c27f5
obdrza <obdrzalkova.eliska@gmail.com>
2017-12-07 15:26:28 +0100

    pokus

--- config.R
+++ config.R
@@ -1,7 +1,7 @@
 library(git2r)
-getwd()
+aaa=getwd()
 
-repo = repository("C:/Users/Evka2/Desktop/ped_23_11/PED_web")
-config(repo, user.name = "xzvoe001", user.email = "zvonarova.eva@gmail.com")
+repo = repository(aaa)
+config(repo, user.name = "obdrza", user.email = "obdrzalkova.eliska@gmail.com")
 
 
